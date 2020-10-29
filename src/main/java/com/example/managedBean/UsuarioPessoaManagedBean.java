79d5adb4a299432b21bcc4b4c6fca965c3711a6a
flavioov <oliveira.v653@gmail.com>
2020-10-29 14:26:28 -0300

    jsf csv export working

--- src/main/java/com/example/managedBean/UsuarioPessoaManagedBean.java
+++ src/main/java/com/example/managedBean/UsuarioPessoaManagedBean.java
@@ -2,6 +2,12 @@ package com.example.managedBean;
 
 import com.example.dao.DaoUsuario;
 import com.example.model.entity.UsuarioPessoa;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
 import org.primefaces.model.chart.BarChartModel;
 
 import javax.annotation.PostConstruct;
@@ -81,5 +87,19 @@ public class UsuarioPessoaManagedBean {
 
 		return "";
 	}
+	public void postProcessXLS(Object document) {
+		HSSFWorkbook wb = (HSSFWorkbook) document;
+		HSSFSheet sheet = wb.getSheetAt(0);
+		CellStyle style = wb.createCellStyle();
+		style.setFillBackgroundColor(IndexedColors.AQUA.getIndex());
+
+		for (Row row : sheet) {
+			for (Cell cell : row) {
+				cell.setCellValue(cell.getStringCellValue().toUpperCase());
+				cell.setCellStyle(style);
+			}
+		}
+	}
+
 
 }
